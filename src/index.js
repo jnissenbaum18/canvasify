78586cfcab51066dce3f92f214a7305f61dc4924
jnissenbaum18 <jnissenbaum@verizon.net>
2018-02-09 19:23:59 -0500

    updates to packages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, Link, IndexLink} from 'react-router';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let middleware = [ thunk, logger ];
+
+
+
+ReactDOM.render(
+	<App />, 
+
+	document.getElementById('root')
+);
+
 registerServiceWorker();
